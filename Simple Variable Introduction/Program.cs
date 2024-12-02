171d857a839d72014a57698dd1968cbce5f2280c
Yash Doshi <yash.doshi@cctech.co.in>
2024-12-02 14:24:37 +0530

    Add project files.

---
+++ Simple Variable Introduction/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Simple_Variable_Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int variable = 10;
+            Console.WriteLine(variable);
+        }
+    }
+}
