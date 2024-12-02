d1585005469864707820f265acc8335eaece588c
Yash Doshi <yash.doshi@cctech.co.in>
2024-12-02 14:30:06 +0530

    Add proj

---
+++ Fixing Simple Bugs/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Fixing_Simple_Bugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+            int num3 = 30;
+            int average = (num1 + num2 + num3) / 3;
+            Console.WriteLine("The average is: " + average);
+        }
+    }
+}
