50ef66f0ae64c16fa19cb4570831b9d1bf7dc46d
Yash Doshi <yash.doshi@cctech.co.in>
2024-12-03 11:45:36 +0530

    Add projects

--- Cats & Food/Program.cs
+++ Cats & Food/Program.cs
@@ -1,18 +1,36 @@
-﻿namespace Cats___Food
+﻿using System;
+
+namespace Cats___Food
 {
     internal class Program
     {
         public static int NotHungryCats(string kitchen)
         {
-            int index1 = kitchen.IndexOf('F');
+            int indexOfF = kitchen.IndexOf('F');
+            int notHungryCats = 0;
 
+            for (int i = 0; i < indexOfF; i += 2)
+            {
+                if (i + 1 < kitchen.Length && kitchen.Substring(i, 2) == "O~")
+                {
+                    notHungryCats++;
+                }
+            }
 
+            for (int j = indexOfF + 2; j < kitchen.Length - 1; j += 2)
+            {
+                if (kitchen.Substring(j, 2) == "~O")
+                {
+                    notHungryCats++;
+                }
+            }
 
-            return index1;
+            return notHungryCats;
         }
+
         static void Main(string[] args)
         {
-            Console.WriteLine(NotHungryCats("~O~O~O~O F"));
+            Console.WriteLine(NotHungryCats("~O~O F ~O~O"));
             Console.ReadKey();
         }
     }
---
+++ Extracting email addresses with Regex/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Extracting_email_addresses_with_Regex
+{
+    internal class Program
+    {
+        public void ExtractPatterns(string input)
+        {
+            string pattern = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-_]+\.[a-zA-Z]{2,}";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.ExtractPatterns("Contact us at support@example.com or sales@example.org.");
+        }
+    }
+}
